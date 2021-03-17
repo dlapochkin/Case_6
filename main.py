cf80e9c170a69f662c914a454e3741d15cbe9a84
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2021-03-17 10:01:10 +0700

    natural

--- main.py
+++ main.py
@@ -27,3 +27,19 @@ def color(c):
             print(c, 'не является верным значением.',end = ' ')
             continue
 print(color(c))
+
+n = 0
+quantity = 0
+n = input('Пожалуйста, введите количество шестиугольников, располагаемых в ряд: ')
+def correct_quantity(n):
+    while True:
+        if n.isdigit() == True:
+            n = int(n)
+            if n > 3 and n < 21:
+                quantity = n
+                return quantity
+            else:
+                print('Оно должно быть от 4 до 20.', end=' ')
+                n = input('Пожалуйста, повторите попытку: ')
+                continue
+print(correct_quantity(n))
