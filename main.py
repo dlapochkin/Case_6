e65594b891fc5b04ad831d447f773035f4230ce8
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2021-03-17 09:35:26 +0700

    natural

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print(1)
+c = 0
+def color(c):
+    while True:
+        c = input('Пожалуйста, введите цвет: ')
+        if c == 'зеленый':
+            c = 'green'
+            return c
+        elif c == 'красный':
+            c = 'red'
+            return c
+        elif c == 'синий':
+            c = 'blue'
+            return c
+        elif c == 'оранжевый':
+            c = 'orange'
+            return c
+        elif c == 'желтый':
+            c = 'yellow'
+            return c
+        elif c == 'пурпурный':
+            c = 'purple'
+            return c
+        elif c == 'розовый':
+            c = 'pink'
+            return c
+        else:
+            print(c, 'не является верным значением.',end = ' ')
+            continue
+print(color(c))
